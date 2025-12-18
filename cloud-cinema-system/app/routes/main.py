6728c84910d82cb85a4744d51ceabdc1ae82c688
ceassen <chuneassen@gmail.com>
2025-12-18 12:43:49 +0800

    Show homepage

--- cloud-cinema-system/app/routes/main.py
+++ cloud-cinema-system/app/routes/main.py
@@ -5,4 +5,7 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return "<h1>Cloud Cinema Homepage</h1><p>If you see this, the Modular Structure is working!</p>"
+    return render_template('homepage.html')
+
+
+
